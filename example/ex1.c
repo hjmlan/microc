79505d50dd48108591ccb835dcc64df13d2f59f9
an <hu_jiaan@outlook.com>
2022-06-09 05:10:55 +0800

    changed different comment way by adjust CLex.fsl

--- example/ex1.c
+++ example/ex1.c
@@ -15,7 +15,8 @@ void main(int n) {
   // h[12] = 5;
   // 数组越界，程序的行为会异常
   // 此时解释器，栈式虚拟机，x86程序表现各不相同，可以思考原因
-
+ (*let me show that 
+ another way commet*)
   while (n > 0) {
     print n;
     n = n - 1;
